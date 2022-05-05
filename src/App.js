0c0422998ab82604c92dd02be55ee4fb5f5b29ad
Trajan Miller <trajanmiller324@gmail.com>
2022-05-05 19:16:10 -0400

    Save task to task array in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import { Component } from "react";
 import "./App.css";
+import Overview from "./components/Overview";
 
-function App() {
-  return <div></div>;
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: [],
+    };
+
+    this.addTask = this.addTask.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    this.setState({ input: e.target.value });
+  }
+
+  addTask(e) {
+    const { task, input } = this.state;
+    e.preventDefault();
+    task.push(input);
+    console.log(task);
+    this.setState({ input: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.addTask}>
+          <input
+            onChange={this.onChange}
+            type="text"
+            name="task"
+            id="task"
+            placeholder="Enter a task"
+            required
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview task={this.state.task} />
+      </div>
+    );
+  }
 }
 
 export default App;
