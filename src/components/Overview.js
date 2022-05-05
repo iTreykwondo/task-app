decb86c7d6ea73edcf2abd5f97598be053a09e73
Trajan Miller <trajanmiller324@gmail.com>
2022-05-05 19:17:01 -0400

    Loop over task array and render task items

---
+++ src/components/Overview.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class Overview extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.task.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Overview;
